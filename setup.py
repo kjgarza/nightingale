34ff59c697a3a2a0ea74f4e240fb203174d74f72
kjgarza <kgarza@datacite.org>
2023-05-14 08:29:35 +0200

    new setup files

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nightingale",
+    version="0.0.1",
+    description="A Python package for prioritizing GitHub issues using the Command pattern",
+    author="Kristian Garza",
+    author_email="kj.garza@gmail.com",
+    url="https://github.com/kjgarza/nightingale",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    entry_points={
+        "console_scripts": [
+            "nightingale=nightingale.cli:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
